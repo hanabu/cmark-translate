2f89b8114925f1cde3967bde9a51c7ce707537e1
Hanabusa Masahiro <masa@hanabusa.net>
2022-10-26 04:43:35 +0900

    Add missing exports

--- src/lib.rs
+++ src/lib.rs
@@ -7,5 +7,6 @@ pub use cmark_xml::{
     cmark_from_xml, cmark_from_xmldom, read_cmark_with_frontmatter, xml_from_cmark,
     xmldom_from_cmark,
 };
-pub use deepl::{Deepl, DeeplGlossary};
+pub use deepl::{Deepl, DeeplGlossary, Language};
+pub use glossary::read_glossary;
 pub use trans::{translate_cmark, translate_cmark_file, translate_toml};
